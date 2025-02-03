faab710282fda0acf8cbfb0821ef506abfdbf880
kazak1155 <kazakmt4@gmail.com>
2025-02-03 10:14:10 +0300

    добавил модель Room и теперь делаю, вывод наличия комнат в отеле

---
+++ app/Http/Controllers/Room/ShowController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Room;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShowController extends Controller
+{
+    public function showRoomInHotel()
+    {
+        return 111;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\Hotel\StoreController;
-//use App\Http\Controllers\Room\ShowRoomController;
 use App\Http\Controllers\Hotel\DeleteController;
 use App\Http\Controllers\Hotel\UpdateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Hotel\ShowController;
+use App\Http\Controllers\Room\ShowController as RoomShowController;;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,3 +19,6 @@ Route::get('hotel/{hotel}', [ShowController::class, 'show'])->name('show.hotel')
 Route::delete('hotel/{hotel}', [DeleteController::class, 'delete'])->name('delete.hotel');
 Route::post('hotel_store', [StoreController::class, 'store'])->name('store.hotel');
 Route::patch('hotel/{hotel}', [UpdateController::class, 'update'])->name('update.hotel');
+
+
+Route::get('room/{room}', [RoomShowController::class, 'showRoomInHotel'])->name('show.Room');
